603a46715f6180e7ba114a30152e6f8271bd2594
Goswami <vinodpuri.goswami@refinitiv.com>
2022-07-28 10:55:07 +0530

    Updates

---
+++ precache-manifest.eb50105c5f5e54cbc57284993302b31a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "08f060bd6a890976a83a",
+    "url": "/portfolio/static/css/main.fc61d510.chunk.css"
+  },
+  {
+    "revision": "08f060bd6a890976a83a",
+    "url": "/portfolio/static/js/main.08f060bd.chunk.js"
+  },
+  {
+    "revision": "5aeb6c39d1c1dde638dc",
+    "url": "/portfolio/static/css/1.e079644d.chunk.css"
+  },
+  {
+    "revision": "5aeb6c39d1c1dde638dc",
+    "url": "/portfolio/static/js/1.5aeb6c39.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "e55af29b7281f3650af47e091c8242ff",
+    "url": "/portfolio/static/media/cv.e55af29b.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/portfolio/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/portfolio/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "355a91bf9e09a9720856a2dbce1ab692",
+    "url": "/portfolio/static/media/portfolio1.355a91bf.jpg"
+  },
+  {
+    "revision": "df7ae32fafd2b55756d67fa8e93a2fac",
+    "url": "/portfolio/index.html"
+  }
+];
